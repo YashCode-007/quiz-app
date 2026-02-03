70874a58dc024c110afb3a89d1679e443465931f
ash <vanshraj0987@gmail.com>
2026-02-03 13:18:58 +0530

    FrontEnd Completed

---
+++ java-quiz.js
@@ -0,0 +1,77 @@
+let currentQuestionIndex = 0;
+let questions = [];
+let correctAnswers = 0;
+let totalquestions = 0;
+
+// Function to fetch questions from api
+async function fetchQuestions() {
+    try {
+        const response = await fetch('localhost:8080/api/questions');
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        questions = await response.json();
+        totalquestions = questions.length;
+
+        document.getElementById('total-questions').textContent = totalquestions;
+
+        displayQuestion();
+
+    } catch (error) {
+        console.error('Error fetching questions: ', error);
+        document.getElementById('question-text').textContent = "Failed to load questions. Please try again.";
+    }
+
+}
+
+function displayQuestion() {
+    if (questions.length === 0) return;
+
+    const questionText = document.getElementById('question-text');
+    const optionsContainer = document.getElementById('options-container');
+    const currentQuestion = questions[currentQuestionIndex];
+
+    // THIS LINE MIGHT CHANGE ACC TO THE OBJECT OF THE QUESTION
+    questionText.textContent = currentQuestion.question;
+    optionsContainer.innerHTML = '';
+
+    currentQuestion.options.forEach((option) => {
+        const button = document.createElement("button");
+        button.textContent = option;
+        // YOU CAN CHANGE IT TO CLASSNAME IF ERROR OCCURS
+        button.classList.add('option');
+        button.onclick = () => checkAnswer(option);
+        optionsContainer.appendChild(button);
+    });
+
+    document.getElementById('current-question').textContent = currentQuestionIndex + 1;
+
+}
+
+function checkAnswer(selectedOption) {
+    const currentQuestion = questions[currentQuestionIndex];
+
+    // Increment if the answer is correct
+    if (selectedOption === currentQuestion.correctAnswer) {
+        correctAnswers++;
+    }
+
+    // Automatically move to the next Question after checking the answer
+    nextQuestion();
+}
+
+function nextQuestion() {
+    if (currentQuestionIndex < questions.length - 1) {
+        displayQuestion();
+    }
+    else {
+
+        localStorage.setItem('quizScore', correctAnswers);
+        localStorage.setItem('totalQuestions', totalquestions);
+        window.location.href = 'result.html';
+    }
+}
+
+window.onload=fetchQuestions;
